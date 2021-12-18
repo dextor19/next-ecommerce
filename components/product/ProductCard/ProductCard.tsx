dee558a74f90099e19c1bd421fdd3c5b0fc1cc8d
Dexter Griffiths <dexgriffiths@hotmail.co.uk>
2021-12-19 00:48:35 +0900

    product card title links to specific product page

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -80,11 +80,13 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
     return (
     <div className={styles.card}>
         <Link href={`/products/${product.id}`} passHref>
-            <Image src={product.image} height={584} width={304} alt="" />
+            <Image src={product.image} title={product.product} height={584} width={304} alt="" />
         </Link>
         <div className={styles.row}>
             <div className={styles.column}>
-                <h4 className={styles.title}>{product.product}</h4>
+                <Link href={`/products/${product.id}`} passHref>
+                    <h4 className={styles.title}>{product.product}</h4>
+                </Link>
                 <p className={styles.price}>$ {product.price}</p>
                 {newArrival ? <p className={styles.price}>New Arrival</p> : null}
             </div>
